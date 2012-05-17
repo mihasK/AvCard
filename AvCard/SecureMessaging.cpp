e0e7b39b7092a0ab467c244664d0c490002d9ed3
mihasK <mihas-91@tut.by>
2012-05-18 00:40:39 +0300

    реквестер респонсер

--- AvCard/SecureMessaging.cpp
+++ AvCard/SecureMessaging.cpp
@@ -1,12 +1,7 @@
 #include "SecureMessaging.h"
 #include <cstring>
 
-#define SYNCHRO_LENGTH				BELT_SYNCHRO_LENGHT
-#define IMITO_LENGTH				BELT_MAC_LENGHT
-#define PACE_GENPOINT_PROTOTYPE_SIZE	32
-#define PACE_KEY_EFFICIENT_LENGHT		32
-#define PACE_REQUEST_1_SIZE		PACE_GENPOINT_PROTOTYPE_SIZE/2
-#define PACE_REQUEST_2_SIZE		BIGN_POINT_LENGHT+IMITO_LENGTH
+
 
 bool checkImito(byte* message, int size, byte* key){
 	byte* imitoCmp=new byte[IMITO_LENGTH];
@@ -20,7 +15,7 @@ void refreshSubKey(byte* keyMain, byte* key1, byte* key2){
 	belt_keyrep(keyMain, 1, key2);
 }
 void SecureMessaging::update(byte* shift){
-	brng_hmac(keyMain, shift, keyMain,(int)BELT_KEY_LENGHT);
+	brng_hmac(keyMain, shift, keyMain,SM_KEY_SHIFT_LENGTH);
 	//change keyEncr, keyMAC
 	refreshSubKey(keyMain,keyEncr,keyMAC);
 }
@@ -40,8 +35,8 @@ void SecureMessaging::unwrap(byte* message,int length, byte* to){
 bool SecureMessaging::setTemplate(Password* pwd){
 	this->pwd=pwd;
 }
-void SecureMessaging::response1(byte* request, byte* to ){ 
-	byte* paceKeyPrototype=new byte[PACE_GENPOINT_PROTOTYPE_SIZE];
+void SMResponser::response1(byte* request, byte* to ){ 
+	 byte* paceKeyPrototype=new byte[PACE_GENPOINT_PROTOTYPE_SIZE];
 	 byte* key= new byte[BELT_HASH_LENGHT];
 	 belt_hash (pwd -> data,pwd->length, key);
 	 belt_ecb_decr (request, PACE_REQUEST_1_SIZE, key, paceKeyPrototype); 
@@ -62,7 +57,7 @@ void SecureMessaging::response1(byte* request, byte* to ){
 	 delete[BIGN_POINT_LENGHT] point;
 } 
 // Va||Ta
-void SecureMessaging::response2(byte* request, byte* to) { 
+void SMResponser::response2(byte* request, byte* to) { 
 	if(!checkImito(request, PACE_REQUEST_2_SIZE,keyMAC))
 		throw "imito not match";
 	byte * extPoint = request;
@@ -74,7 +69,25 @@ void SecureMessaging::response2(byte* request, byte* to) {
     refreshSubKey(keyMain,keyEncr,keyMAC);
 }
 
-
+void SMRequester::request1(byte* to){
+	byte* prototype=new byte[PACE_GENPOINT_PROTOTYPE_SIZE/2];
+	rng->get(prototype,PACE_GENPOINT_PROTOTYPE_SIZE/2);
+	byte* key= new byte[BELT_HASH_LENGHT];
+	belt_hash (pwd -> data,pwd->length, key);
+	belt_ecb_ecnr (prototype, PACE_REQUEST_1_SIZE, key, to); 
+	delete[BELT_HASH_LENGHT] key;
+	delete[PACE_GENPOINT_PROTOTYPE_SIZE/2] prototype;
+}
+		// pubPoint||imito  ... <- prototype||pubPoint
+void SMRequester::request2(byte* response, byte* to){
+	byte* prototype=new byte[PACE_GENPOINT_PROTOTYPE_SIZE/2];
+	rng->get(prototype,PACE_GENPOINT_PROTOTYPE_SIZE/2);
+	byte* key= new byte[BELT_HASH_LENGHT];
+	belt_hash (pwd -> data,pwd->length, key);
+	belt_ecb_ecnr (prototype, PACE_REQUEST_1_SIZE, key, to); 
+	delete[BELT_HASH_LENGHT] key;
+	delete[PACE_GENPOINT_PROTOTYPE_SIZE/2] prototype;
+}
 
 SecureMessaging::SecureMessaging(void)
 {
--- AvCard/SecureMessaging.h
+++ AvCard/SecureMessaging.h
@@ -6,13 +6,19 @@
 #include "password.h"
 #include "helpers.h"
 
-
+#define SM_KEY_SHIFT_LENGTH			BELT_KEY_LENGHT
+#define SYNCHRO_LENGTH				BELT_SYNCHRO_LENGHT
+#define IMITO_LENGTH				BELT_MAC_LENGHT
+#define PACE_GENPOINT_PROTOTYPE_SIZE	32
+#define PACE_KEY_EFFICIENT_LENGHT		32
+#define PACE_REQUEST_1_SIZE		PACE_GENPOINT_PROTOTYPE_SIZE/2
+#define PACE_REQUEST_2_SIZE		BIGN_POINT_LENGHT+IMITO_LENGTH
 
 
 class SecureMessaging :
 	public CardObject
 {
-private:
+protected:
 	Password* pwd;
 	RNG* rng;
 	//BELT_KEY_LENGHT
@@ -24,10 +30,7 @@ private:
 public:
 	//[ cmd :< MSE : Set  AT >] 
 	bool setTemplate(Password* pwd);
-	//[ cmd :< MSE : General    Authenticate >]
-    void response1 (byte* request, byte* to );
-	//[ cmd :< MSE : General    Authenticate >] 
-	void response2 (byte* request, byte* to);
+	
 	//shift ~ BELT_KEY_LENGHT(32)
 	void update(byte* shift);
 	void wrap(byte* message,int length, byte* to );
@@ -35,4 +38,19 @@ public:
 	SecureMessaging(void);
 	~SecureMessaging(void);
 };
+
+class SMResponser:public SecureMessaging{
+	//[ cmd :< MSE : General    Authenticate >]
+    void response1 (byte* request1, byte* to );
+	//[ cmd :< MSE : General    Authenticate >] 
+	void response2 (byte* request2, byte* to);
+}
+
+class SMRequester:public SecureMessaging{
+	void request1 (byte* to );
+	//[ cmd :< MSE : General    Authenticate >]
+    void request2 (byte* response1, byte* to );
+	//[ cmd :< MSE : General    Authenticate >] 
+	void request3 (byte* response2, byte* to);
+}
 
