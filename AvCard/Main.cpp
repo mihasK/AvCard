fc266db55ebb1b94ada7fb6ae0d44562519df2ef
mihasK <mihas-91@tut.by>
2012-05-20 17:34:54 +0300

    main

--- AvCard/Main.cpp
+++ AvCard/Main.cpp
@@ -1,34 +1,17 @@
 #pragma once
 #include <iostream>
-#include "CardObject.h"
-#include "RNG.h"
-#include "Timer.h"
-#include <ctime>
 #include "tests.h"
 
 
 using namespace std;
 
-void testTimer(){
-	Timer timer=Timer();
-	char* time = asctime(timer.get());
-	cout << "Current time: " << time << endl;
-		system("PAUSE");
-}
 
 int main(){
-	/*RNG rng;
-	cout << "Hello world"<<endl;
-	byte * bytes= new byte[100];
-	rng.get(bytes,100);
-	for(int i=0;i<100;i++)
-		cout<<(int)bytes[i]<<" ";
-	cout<<endl;
-	system("PAUSE");
-	delete[] bytes;*/
 	tests::belt_test();
-	//testTimer();
+	tests::testRNG();
+	tests::testTimer();
+	tests::testPACE();
 	return 0;
 
-	
+
 }
